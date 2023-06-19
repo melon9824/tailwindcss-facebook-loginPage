6054426e6c83c3bd11066dfd7a7ea5a1b1696c89
khorshed-alam <ka.melon1968@gmail.com>
2023-06-19 11:22:44 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'body-bg-color': '#F0F2F4',
+        'form-bg-color': '#FFFFFF',
+        'create-button-bg-color': '#3BB829',
+        'create-button-text-color': '#B3E6AD',
+        'login-button-bg-color': '#1676F2',
+        'text-color': '#545658',
+        'input-border-color': '#f1f1f1',
+        'white-color': '#ffffff'
+      },
+    },
+  },
+  plugins: [],
+}
+
